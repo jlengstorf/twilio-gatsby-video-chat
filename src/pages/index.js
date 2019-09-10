e02df388c53b52c3f8e14aaacbda05d4ed98cae8
Jason Lengstorf <jason@lengstorf.com>
2019-09-09 17:19:23 -0700

    feat: only show the join form on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from '../components/layout';
 import Join from '../components/join';
-import useTwilioVideo from '../hooks/use-twilio-video';
 
-const Video = ({ token }) => {
-  const { videoRef } = useTwilioVideo(token);
-
-  return <div className="chat" ref={videoRef} />;
-};
-
-export default () => {
-  const [token, setToken] = useState(false);
-
-  return (
-    <Layout>
-      {token ? <Video token={token} /> : <Join storeToken={setToken} />}
-    </Layout>
-  );
-};
+export default ({ location }) => (
+  <Layout>
+    <Join location={location} />
+  </Layout>
+);
