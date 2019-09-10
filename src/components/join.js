1c777e3f3dc3b207294f0a2acee4c2d3f6b94481
Jason Lengstorf <jason@lengstorf.com>
2019-09-09 17:17:53 -0700

    feat: use the context to join a room

--- src/components/join.js
+++ src/components/join.js
@@ -1,44 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import useTwilioVideo from '../hooks/use-twilio-video';
+import { navigate } from 'gatsby';
 
-const Join = ({ storeToken }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+const Join = ({ location }) => {
+  const defaultRoom = (location && location.state && location.state.room) || '';
+  const { getParticipantToken, room: roomName, token } = useTwilioVideo();
+  const [identity, setIdentity] = useState('');
+  const [room, setRoom] = useState(defaultRoom);
+
+  useEffect(() => {
+    if (token && roomName) {
+      navigate(`/room/${roomName}`);
+    }
+  }, [token, roomName]);
 
   const handleSubmit = async event => {
     event.preventDefault();
 
-    const result = await axios({
-      method: 'POST',
-      url: 'https://jasmine-greyhound-8600.twil.io/create-room-token',
-      data: { identity: name, room },
-    });
-
-    storeToken(result.data);
+    getParticipantToken({ identity, room });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="start-form">
-      <label htmlFor="name">
-        Display name:
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-      </label>
-      <label htmlFor="room">
-        Name Your Chat Room
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={e => setRoom(e.target.value)}
-        />
-      </label>
-      <button type="submit">Join Video Chat</button>
-    </form>
+    <>
+      <h1>Start or Join a Video Chat</h1>
+      <form onSubmit={handleSubmit} className="start-form">
+        <label htmlFor="identity">
+          Display name:
+          <input
+            type="text"
+            id="identity"
+            value={identity}
+            onChange={e => setIdentity(e.target.value)}
+          />
+        </label>
+        <label htmlFor="room">
+          Which room do you want to join?
+          <input
+            type="text"
+            id="room"
+            value={room}
+            onChange={e => setRoom(e.target.value)}
+          />
+        </label>
+        <button type="submit">Join Video Chat</button>
+      </form>
+    </>
   );
 };
 
