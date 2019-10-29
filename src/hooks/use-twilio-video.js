54c6731dd1a40a17fe395e5969242ad670764415
Jason Lengstorf <jason@lengstorf.com>
2019-10-29 12:06:19 -0700

    feat: add single-file state management

---
+++ src/hooks/use-twilio-video.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+const DEFAULT_STATE = {
+  identity: false,
+  roomName: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'join':
+      return { ...state, identity: action.identity, roomName: action.roomName };
+
+    default:
+      console.warn(`Invalid action type “${action.type}” called.`);
+      return DEFAULT_STATE;
+  }
+};
+
+const TwilioVideoContext = createContext();
+
+const TwilioVideoProvider = ({ children }) => (
+  <TwilioVideoContext.Provider value={useReducer(reducer, DEFAULT_STATE)}>
+    {children}
+  </TwilioVideoContext.Provider>
+);
+
+export const wrapRootElement = ({ element }) => (
+  <TwilioVideoProvider>{element}</TwilioVideoProvider>
+);
+
+const useTwilioVideo = () => {
+  const [state, dispatch] = useContext(TwilioVideoContext);
+
+  return { state, dispatch };
+};
+
+export default useTwilioVideo;
